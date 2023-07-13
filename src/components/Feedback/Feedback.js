04437e369e2adcc704d320c079a9ff237ec19773
Dmitriy Gonharov <my.webpage057@gmail.com>
2023-07-13 23:27:59 +0300

    update

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -2,33 +2,33 @@ import React, {Component} from "react";
 // import PropTypes from "prop-types";
 
 class Feedback extends Component {
-
     state = {
-        good: 0,
-        neutral: 0,
-        bad: 0
-      }
-
-    handleClick = () => {
-        console.log("click");
-        this.setState(prevState=> ({
-            good: prevState.value +1
-        }))
+        value: 0
     }
+// метод класса
+handleIncrement = ()=> {
+    console.log("Click on button");
+    this.setState(prevState => {
+        value: prevState +1
+    })
+}
 render() {
     return (
        <>
-       <h2>Please leave feedback</h2>
-       <button onClick={this.handleClick}>Good</button>
-       <button onClick={this.handleClick}>Neutral</button>
-       <button onClick={this.handleClick}>Bad</button>
-       <h2>Statistics</h2>
+        <div>
+            <h2>Please leave feedback</h2>
+            <button type="button" onClick={this.handleIncrement}>Good</button>
+            <button type="button" onClick={this.handleIncrement}>Neutral</button>
+            <button type="button" onClick={this.handleIncrement}>Bad</button>
+            <h2>Statistics</h2>
+        </div>
+
        <ul>
-        <li>Good:<span>{this.state.good}</span></li>
-        <li>Neutral: {this.state.neutral}</li>
-        <li>Bad:<span>{this.state.bad}</span></li>
-        <li>Total:<span>amount</span></li>
-        <li>Possitive feedback:<span>amount</span></li>
+            <li>Good:{this.state.value}</li>
+            <li>Neutral:{this.state.value}</li>
+            <li>Bad:{this.state.value}</li>
+            <li>Total:<span>amount</span></li>
+            <li>Possitive feedback:<span>amount</span></li>
        </ul>
        </> 
     )
